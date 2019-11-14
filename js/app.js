502f23bb3a27b9288fbb5a53cd8bc4bf6cb6c1a9
JulieAllix <allixjulie@gmail.com>
2019-11-14 18:59:56 +0100

    changement de projet pour faire un casse-briques

--- js/app.js
+++ js/app.js
@@ -1,44 +1,41 @@
 var app = {
     init: function(){
-        
         // We get the start button and add an event to it
         let buttonElement = document.querySelector('#start');
         buttonElement.addEventListener("click", app.handleButtonClick);
 
-        //let buttonElementClean = document.querySelector('#clean');
-        //buttonElementClean.addEventListener('click', function(){
-            //console.log('test');
-        //});
+        let buttonElementClean = document.querySelector('#jump');
+        buttonElementClean.addEventListener('click', app.erasePlayerZone);
+    
     },
     handleButtonClick:function(){
         app.drawModel('player');
     },
     drawModel: function(name){
         // We first erase the gaming zone
-        app.erasePlayerZone();
-        event.preventDefault();
+        // app.erasePlayerZone();
+        //event.preventDefault();
         // We get the content of the model with the character name
         let model = drawings.characters[name];
-        console.log('fail');
         //For each model element, I get the line in the modelLine variable
         for (let modelLine of model) {
             app.drawLine(modelLine);
-            
         }
     },
-    erasePlayerZone: function(){
+    /*erasePlayerZone: function(){
         // I select the game zone
+        console.log("test");
         let playerZone = document.querySelector('#player-zone');
         // I empty its content
-        playerZone.innerText = "";
-    },
+        playerZone.innerContent = "";
+    },*/
     drawLine: function(line) {
         // I select the player zone
         let playerZone = document.querySelector('#player-zone');
         // for each character of the line, we create a pixel element and we add the class corresponding to the character. Then we add the pixel elt to the player zone
         for (let character of line) {
             let pixelElement = document.createElement('div');
-            pixelElement.innerText = character;
+            //pixelElement.innerText = character;
             let className = app.getCharacterClassName(character);
             pixelElement.classList.add(className);
             console.log(pixelElement);
--- php/index.php
+++ php/index.php
@@ -4,18 +4,62 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Jump for your life</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <title>Casse-moi tout là-dedans !</title>
+    <style>
+    	* { padding: 0; margin: 0; }
+    	canvas { background: #eee; display: block; margin: 0 auto; }
+    </style>
+    <!--<link rel="stylesheet" href="../css/style.css">-->
 </head>
 <body>
-    <div id="game-zone">
-        <div id="player-zone">
-        </div>
-    </div>
-    <button id="start" type="button">Start</button>
-    <button id="clean" type="button">Clean</button>
-    
-    <script src="../js/drawings.js"></script>
-    <script src="../js/app.js"></script>
+    <canvas id="myCanvas" width="480" height="320"></canvas>
+
+    <script>
+        var canvas = document.getElementById("myCanvas");
+        var ctx = canvas.getContext("2d");
+        var x = canvas.width/2;
+        var y = canvas.height-30;
+        var dx = -3;
+        var dy = -1;
+        var ballRadius = 10;
+        var randomColor = "#0095DD";
+
+        function drawBall(color) {
+            ctx.beginPath();
+            ctx.arc(x, y, 10, 0, Math.PI*2);
+            ctx.fillStyle = color;
+            ctx.fill();
+            ctx.closePath();
+            ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+        }
+
+        function draw() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            drawBall(randomColor);
+            x += dx;
+            y += dy;
+
+            if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+                dx = -dx;
+                randomColor = getRandomColor();
+            }
+            if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+                dy = -dy;
+                randomColor = getRandomColor();
+            }
+        }
+
+        function getRandomColor() {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+            }
+
+        setInterval(draw, 30);
+    </script>
 </body>
 </html>
